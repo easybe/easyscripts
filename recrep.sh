b49367329fdf26153b8322d92de9f71e246a4d01
EasyB <spam@easyb.ch>
2011-11-10 15:06:44 +0100

    fixed usage

--- recrep.sh
+++ recrep.sh
@@ -2,19 +2,21 @@
 
 # easyb 2011
 
-if [ $# -lt 3 ]; then
-    echo "usage: `basename $0` OLD NEW [-d]"
+if [ $# -lt 2 ]; then
+    echo "usage: `basename $0` OLD NEW [-s]"
     exit
 fi
 
 OLD=$1
 NEW=$2
-OPT=$3
+if [  "x$3" != "x"  ]; then
+    OPT="[SIM] "
+fi
 
 grep -r -l --exclude-dir=.svn $OLD * |
     while read FILE
     do
-        echo "processing: $FILE"
+        echo "${OPT}processing: $FILE"
 
         if [ "x$OPT" == "x" ]; then
             sed "s/$OLD/$NEW/i;" "$FILE" >"$FILE.tmp"
