b35b2c95a2d4e389d189c733c1677929248d0677
easyb <spam@easyb.ch>
2012-06-24 18:46:17 +0200

    epren: clean up a little

--- epren.py
+++ epren.py
@@ -20,7 +20,6 @@ from xml.dom import minidom
 class Main(object):
 
     def __init__(self, argv = None):
-
         usage = "usage: %prog [options] [name of show]"
         parser = optparse.OptionParser(usage=usage)
         parser.add_option("-d", "--dry-run",
@@ -57,8 +56,35 @@ class Main(object):
             print "*** Dry run requested ***"
 
     def run(self):
+        self._fetchShows()
+        self._show = self._shows[0]
+        showName = self._getVal(self._show, "name")
+        answ = 'n'
+        if self._seasonNo is not None:
+            answ = self._prompt("Rename '{0}' season {1:d} ?".format(
+                showName, self._seasonNo), 'y')
 
-        self._fetchNames()
+        while answ != 'y':
+            i = 0
+            for show in self._shows:
+                name = self._getVal(show, "name")
+                print u"[{0:d}] {1}".format(i, name)
+                i += 1
+            print "[q] Never mind..."
+
+            showIndex = self._promptForNumber("Select a show")
+            self._show = self._shows[showIndex]
+            showName = self._getVal(self._show, "name")
+            seasonCount = self._getVal(self._show, "seasons")
+            self._seasonNo = self._promptForNumber(
+                "Select a season", str(seasonCount))
+            answ = self._prompt("Rename '{0}' season {1:d} ?".format(
+                showName, self._seasonNo), 'y')
+
+        self._fetchEpisodes()
+        self._rename()
+
+    def _rename(self):
         os.chdir(self._path)
         files = os.listdir(os.getcwd())
         for file in files:
@@ -66,46 +92,26 @@ class Main(object):
             if not m:
                  m = re.search('(\d+)x(\d+)', file)
             if m:
-                #import pdb; pdb.set_trace()
                 key = "{0:d}{1:02d}".format(int(m.group(1)), int(m.group(2)))
                 ext = re.search('(\.\w*)$', file).group(1)
 
-                if self._names.has_key(key):
+                if key in self._names:
                     name = self._names[key] + ext
                     print u"renaming '{0}' to '{1}'".format(file, name)
                     if not self._dry: os.rename(file, name)
 
-    def _fetchNames(self):
-
+    def _fetchShows(self):
         infoUrl = "http://www.tvrage.com/feeds/search.php?show="
-        epUrl = "http://www.tvrage.com/feeds/episode_list.php?sid="
 
         xmlDoc = self._parseUrl(infoUrl + self._searchStr)
-        shows = xmlDoc.getElementsByTagName("show")
-        if len(shows) == 0:
+        self._shows = xmlDoc.getElementsByTagName("show")
+        if len(self._shows) == 0:
             self._exit("Could not find requested show")
 
-        i = 0
-        for show in shows:
-            name = self._getVal(show, "name")
-            print u"[{0:d}] {1}".format(i, name)
-            i += 1
-        print "[q] Never mind..."
+    def _fetchEpisodes(self):
+        epUrl = "http://www.tvrage.com/feeds/episode_list.php?sid="
 
-        index = self._promptForNumber("Select a show")
-
-        show = shows[index]
-        name = self._getVal(show, "name")
-        showId = self._getVal(show, "showid")
-        seasonCount = self._getVal(show, "seasons")
-    
-        if self._seasonNo:
-            default = self._seasonNo
-        else:
-            default = seasonCount
-        self._seasonNo = self._promptForNumber("Select the '{0}' season".format(name),
-                                               str(default))
-
+        showId = self._getVal(self._show, "showid")
         xmlDoc = self._parseUrl(epUrl + showId)
 
         seasons = xmlDoc.getElementsByTagName("Season")
@@ -122,7 +128,8 @@ class Main(object):
         if season:
             episodes = season.getElementsByTagName("episode")
         else:
-            self._exit("No data for season {0:d} available".format(self._seasonNo))
+            self._exit("No data for season {0:d} available".format(
+                self._seasonNo))
 
         self._names = dict()
         for episode in episodes:
@@ -132,26 +139,31 @@ class Main(object):
             title = re.sub('/', '-', title)
             filename = numberStr + " - " + title
             self._names[numberStr] = filename
-            
+
     def _getInfoFromPath(self):
         d = os.path.basename(self._path)
         m = re.search('(.*) - Season (\d*)', d)
         if m:
             return (m.group(1), int(m.group(2)))
-            
+
         return None
-        
-    def _promptForNumber(self, msg, default="0"):
-        answ = ""
+
+    def _promptForNumber(self, msg, default='0'):
+        answ = ''
         while not answ.isdigit():
-            answ = raw_input("{0} [{1}]: ".format(msg, default))
-            if answ == "q":
-                self._exit("bye")
-            if not answ:
-                answ = default
+            answ = self._prompt(msg, default)
+            
+        return int(answ)
+
+    def _prompt(self, msg, default='y'):
+        answ = raw_input("{0} [{1}]: ".format(msg, default))
+        if answ == 'q':
+            self._exit("bye")
+        if not answ:
+            answ = default
         answ = answ.strip()
 
-        return int(answ)
+        return answ
 
     def _getVal(self, node, tag):
         return node.getElementsByTagName(tag)[0].firstChild.nodeValue
