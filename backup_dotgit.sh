68d78d18bfc42a7d3c03a67273eee91388d723a7
EasyB <spam@easyb.ch>
2012-07-09 10:25:13 +0200

    backup_dotgit: only search for .git

--- backup_dotgit.sh
+++ backup_dotgit.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 SRC=/local-home
-DEST= ...
+DEST=/tmp/bu
 
 old=$(ls -1 $DEST | tail -n 1)
 new=$(date +"%Y-%m-%dT%H:%M:%S")
 
 mkdir -p $DEST/$new
 
-find $SRC -type d \( -name ".git" -o -name ".git_externals" \) -print0 | \
+find $SRC -type d -name ".git" -print0 2>/dev/null | \
     rsync -pavrl --link-dest=$DEST/$old --files-from=- --from0 \
     / $DEST/$new
