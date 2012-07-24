8a608ec710992253604f0e4363c61df5422b6d6f
EasyB <spam@easyb.ch>
2012-07-24 11:17:24 +0200

    backup-dotgit: print last backup directory and count of new files

--- backup-dotgit.sh
+++ backup-dotgit.sh
@@ -9,8 +9,14 @@ old=$(ls -1 $DEST | tail -n 1)
 new=$(date +"%Y-%m-%dT%H:%M:%S")
 
 echo "Backing up to $DEST/$new ..."
+echo "Taking $DEST/$old into account."
+
 mkdir -p $DEST/$new
 
 find $SRC -type d -name ".git" -print0 2>/dev/null | \
     rsync -arv --link-dest=$DEST/$old --files-from=- --from0 \
     / $DEST/$new
+
+new_files=$(find $DEST/$new -type f -links 1 | wc -l)
+echo "Backed up $new_files new files"
+echo
