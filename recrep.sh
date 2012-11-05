bc22dea59d2bbf0f216789fbb5ef5e865fc9cf50
EasyB <spam@easyb.ch>
2012-11-05 18:12:04 +0100

    recrep.sh: add -f option

--- recrep.sh
+++ recrep.sh
@@ -3,7 +3,7 @@
 # easyb 2011
 
 if [ $# -lt 2 ]; then
-    echo "usage: `basename $0` OLD NEW [-s|-w]"
+    echo "usage: `basename $0` OLD NEW [-s|-w] [-f FILE]"
     exit
 fi
 
@@ -15,10 +15,14 @@ echo "$@" | grep -q "\-s"
 [ $? -eq 0 ] && sim="true"
 echo "$@" | grep -q "\-w"
 [ $? -eq 0 ] && word="true"
-
-files=$(find . -type f \( ! -regex '.*/\..*' \) | \
-    xargs grep $([ $word ] && echo "-w") -I -l \
-    --exclude={*#*,*.*} --exclude-dir=.* $old 2>/dev/null)
+echo "$@" | grep -q "\-f"
+if [ $? -eq 0 ]; then
+    files=$(echo "$@" | sed -r 's:.*-f ([[:alnum:]\/\.]+).*:\1:g' 2>/dev/null)
+else
+    files=$(find . -type f \( ! -regex '.*/\..*' \) | \
+        xargs grep $([ $word ] && echo "-w") -I -l \
+        --exclude={*#*,*.*} --exclude-dir=.* $old 2>/dev/null)
+fi
 
 for f in $files; do
     [ "$sim" ] && SIM="[SIM] "
@@ -30,8 +34,8 @@ for f in $files; do
             oldRE='\b'$old'\b'
             newRE="$new"
         else
-            oldRE="$new"
-            newRE="$old"
+            oldRE="$old"
+            newRE="$new"
         fi
         sed "s:$oldRE:$newRE:g" "$f" >"$f.tmp" && \
             mv "$f.tmp" "$f"
